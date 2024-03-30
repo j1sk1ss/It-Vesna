f346d53967b8efacaa2a31d286f6a41447f74e8c
soba4at1na <egor.gulih@gmail.com>
2024-03-31 01:21:29 +0500

    beta(deta) navigation admin_panel

--- front/site-body/src/admin_page/AdminPanelPage.js
+++ front/site-body/src/admin_page/AdminPanelPage.js
@@ -1,42 +1,62 @@
-// AdminPanelPage.js
-
-import React from 'react';
-import './AdminPanelPage.css'; // Импорт файлов стилей
+import React, { useState } from 'react';
+import './AdminPanelPage.css';
 
 const AdminPanelPage = () => {
+  const [selectedTab, setSelectedTab] = useState('Заявки');
+  const [moderatorEmail, setModeratorEmail] = useState('');
+  const [moderatorName, setModeratorName] = useState('');
+  const [subTab, setSubTab] = useState('На рассмотрении');
+
+  const handleTabClick = (tabName) => {
+    setSelectedTab(tabName);
+    setSubTab('На рассмотрении'); // Сброс подвкладки при переключении вкладки
+  };
+
+  const handleCreateModerator = () => {
+    console.log('Создание модератора:', moderatorEmail, moderatorName);
+  };
+
   return (
     <div className="admin-panel-page-container">
       <div className="admin-panel-page">
-        {/* Прозрачная полоса с надписью IT Весна */}
         <div className="transparent-bar">
           <div className="it-vesna">IT Весна</div>
         </div>
         
-        {/* Вкладки */}
         <div className="tab-container">
-        <div className="left-tab-container">
-          <div className="tab">Заявки</div>
-          <div className="tab">Модераторы</div>
-          <div className="tab">Номинации</div>
-        </div>
-        <div className="right-tab-container">
-          <div className="tab">На рассмотрении</div>
-          <div className="tab">Принятые</div>
-          <div className="tab">Архив</div>
+          <div className="left-tab-container">
+            <div className={`tab ${selectedTab === 'Заявки' ? 'active' : ''}`} onClick={() => handleTabClick('Заявки')}>Заявки</div>
+            <div className={`tab ${selectedTab === 'Модераторы' ? 'active' : ''}`} onClick={() => handleTabClick('Модераторы')}>Модераторы</div>
+            <div className={`tab ${selectedTab === 'Номинации' ? 'active' : ''}`} onClick={() => handleTabClick('Номинации')}>Номинации</div>
+          </div>
+          <div className="right-tab-container">
+            {selectedTab === 'Заявки' && (
+              <>
+                <div className={`tab ${subTab === 'На рассмотрении' ? 'active' : ''}`} onClick={() => setSubTab('На рассмотрении')}>На рассмотрении</div>
+                <div className={`tab ${subTab === 'Принятые' ? 'active' : ''}`} onClick={() => setSubTab('Принятые')}>Принятые</div>
+                <div className={`tab ${subTab === 'Архив' ? 'active' : ''}`} onClick={() => setSubTab('Архив')}>Архив</div>
+              </>
+            )}
           </div>
         </div>
         
-        {/* Контейнер с содержанием заявки и кнопками */}
-        <div className="request-container">
-          <div className="request">
-            <div className="rectangle-1">
-              <div className="text">Операционная система и ядро ОС</div>
-              <div className="text">Коля</div></div>
-            <div className="button">Подробнее </div>
-            <div className="button save"><img src="ok.png" /></div>
-            <div className="button otmena"><img src="otmena.png" /></div>
-            <div className="button decline"><img src="arh.png" /></div>
+        <div className="tab-content">
+          {selectedTab === 'Заявки' && (
+            <div>
+              {subTab === 'На рассмотрении' && <div>Содержимое подвкладки "На рассмотрении"</div>}
+              {subTab === 'Принятые' && <div>Содержимое подвкладки "Принятые"</div>}
+              {subTab === 'Архив' && <div>Содержимое подвкладки "Архив"</div>}
+            </div>
+          )}
+          {selectedTab === 'Модераторы' && (
+            <div className="input-container">
+            <input type="text" placeholder="Почта" value={moderatorEmail} onChange={(e) => setModeratorEmail(e.target.value)} />
+            <input type="text" placeholder="Имя" value={moderatorName} onChange={(e) => setModeratorName(e.target.value)} />
+            <button className="button-create" onClick={handleCreateModerator}>Создать</button>
           </div>
+          
+          )}
+          {selectedTab === 'Номинации' && <div>Содержимое вкладки "Номинации"</div>}
         </div>
       </div>
     </div>
