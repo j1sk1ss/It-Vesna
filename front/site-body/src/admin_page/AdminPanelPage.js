1b83960cdef48c8a19815c0213bc871673a430f1
soba4at1na <egor.gulih@gmail.com>
2024-04-09 00:07:38 +0500

    fix head mainpage

--- front/site-body/src/admin_page/AdminPanelPage.js
+++ front/site-body/src/admin_page/AdminPanelPage.js
@@ -12,12 +12,12 @@ const AdminPanelPage = () => {
   const [isModeratorsTabSelected, setIsModeratorsTabSelected] = useState(false);
   const [nominationName, setNominationName] = useState('');
   const [nominations, setNominations] = useState(['Номинация 1', 'Номинация 2', 'Номинация 3']);
-  const { requests} = useRequestContext();
+  const { requests } = useRequestContext();
   const { setRequests } = useRequestContext();
 
   const handleDeleteRequest = (id) => {
-      const updatedRequests = requests.filter(request => request.id !== id);
-      setRequests(updatedRequests);
+    const updatedRequests = requests.filter(request => request.id !== id);
+    setRequests(updatedRequests);
   };
 
   const handleTabClick = (tabName) => {
@@ -53,53 +53,53 @@ const AdminPanelPage = () => {
   };
 
   return (
-    <div className="admin-panel-page-container">
-      <div className="admin-panel-page">
-        <div className="transparent-bar">
-          <div className="it-vesna">IT Весна</div>
+    <div className="adminp-panel-page-container">
+      <div className="adminp-panel-page">
+        <div className="adminp-transparent-bar">
+          <div className="adminp-it-vesna">IT Весна</div>
         </div>
         
-        <div className="tab-container">
-          <div className="left-tab-container">
-            <div className={`tab ${selectedTab === 'Заявки' ? 'active' : ''}`} onClick={() => handleTabClick('Заявки')}>Заявки</div>
-            <div className={`tab ${selectedTab === 'Модераторы' ? 'active' : ''}`} onClick={() => handleTabClick('Модераторы')}>Модераторы</div>
-            <div className={`tab ${selectedTab === 'Номинации' ? 'active' : ''}`} onClick={() => handleTabClick('Номинации')}>Номинации</div>
+        <div className="adminp-tab-container">
+          <div className="adminp-left-tab-container">
+            <div className={`adminp-tab ${selectedTab === 'Заявки' ? 'active' : ''}`} onClick={() => handleTabClick('Заявки')}>Заявки</div>
+            <div className={`adminp-tab ${selectedTab === 'Модераторы' ? 'active' : ''}`} onClick={() => handleTabClick('Модераторы')}>Модераторы</div>
+            <div className={`adminp-tab ${selectedTab === 'Номинации' ? 'active' : ''}`} onClick={() => handleTabClick('Номинации')}>Номинации</div>
           </div>
-          <div className="right-tab-container">
+          <div className="adminp-right-tab-container">
             {selectedTab === 'Заявки' && (
               <>
-                <div className={`tab ${subTab === 'На рассмотрении' ? 'active' : ''}`} onClick={() => setSubTab('На рассмотрении')}>На рассмотрении</div>
-                <div className={`tab ${subTab === 'Принятые' ? 'active' : ''}`} onClick={() => setSubTab('Принятые')}>Принятые</div>
-                <div className={`tab ${subTab === 'Архив' ? 'active' : ''}`} onClick={() => setSubTab('Архив')}>Архив</div>
+                <div className={`adminp-tab ${subTab === 'На рассмотрении' ? 'active' : ''}`} onClick={() => setSubTab('На рассмотрении')}>На рассмотрении</div>
+                <div className={`adminp-tab ${subTab === 'Принятые' ? 'active' : ''}`} onClick={() => setSubTab('Принятые')}>Принятые</div>
+                <div className={`adminp-tab ${subTab === 'Архив' ? 'active' : ''}`} onClick={() => setSubTab('Архив')}>Архив</div>
               </>
             )}
           </div>
         </div>
         
-        <div className="tab-content">
+        <div className="adminp-tab-content">
           {selectedTab === 'Заявки' && (
             <div>
               {subTab === 'На рассмотрении' && 
               <div>
-              <div className="RequestWindowPage">
-            <div className="requests1">
-                {requests.map(request => (
-                    <div key={request.id} className="request1">
-                        <div className="request-info">
-                            <div>{request.author}</div>
-                            <div>{request.title}</div>
-                        </div>
-                        <div className="request-buttons">
-                            <Link to={`/request/${request.id}`} className="button more-button"></Link> 
-                            <button className="button delete-button" onClick={() => handleDeleteRequest(request.id)}></button>
-                            <button className="button accept-button"></button>
-                            <button className="button archive-button"></button>
-                        </div>
-                    </div>
-                ))}
-            </div>
-        </div>
-                </div>}
+                <div className="RequestWindowPage">
+                  <div className="requests1">
+                      {requests.map(request => (
+                          <div key={request.id} className="request1">
+                              <div className="request-info">
+                                  <div>{request.author}</div>
+                                  <div>{request.title}</div>
+                              </div>
+                              <div className="request-buttons">
+                                  <Link to={`/request/${request.id}`} className="more-button"></Link> 
+                                  <button className="delete-button" onClick={() => handleDeleteRequest(request.id)}></button>
+                                  <button className="accept-button"></button>
+                                  <button className="archive-button"></button>
+                              </div>
+                          </div>
+                      ))}
+                  </div>
+                </div>
+              </div>}
               {subTab === 'Принятые' && <div>Содержимое подвкладки "Принятые"</div>}
               {subTab === 'Архив' && <div>Содержимое подвкладки "Архив"</div>}
             </div>
@@ -107,19 +107,19 @@ const AdminPanelPage = () => {
           {selectedTab === 'Модераторы' && (
             <div>
               {isModeratorsTabSelected && (
-                <div className="input-container">
+                <div className="adminp-input-container">
                   <input type="text" placeholder="Почта" value={moderatorEmail} onChange={(e) => setModeratorEmail(e.target.value)} />
                   <input type="text" placeholder="Имя" value={moderatorName} onChange={(e) => setModeratorName(e.target.value)} /> 
-                  <button className="button-create" onClick={handleCreateModerator}>Создать</button>
+                  <button className="adminp-button-create" onClick={handleCreateModerator}>Создать</button>
                 </div>
               )}
               <div>
                 {moderators.map((moderator, index) => (
                   <div key={index}>
-                    <div className="moderator-container">
+                    <div className="adminp-moderator-container">
                       <div>{moderator.name}</div>
                       <div>{moderator.email}</div>
-                      <button className="button-delete" onClick={() => handleDeleteModerator(index)}>Удалить</button>
+                      <button className="adminp-button-delete" onClick={() => handleDeleteModerator(index)}>Удалить</button>
                     </div>
                   </div>
                 ))}
@@ -128,16 +128,16 @@ const AdminPanelPage = () => {
           )}
           {selectedTab === 'Номинации' && (
             <div>
-              <div className="input-container">
+              <div className="adminp-input-container">
                 <input type="text" placeholder="Новая номинация" value={nominationName} onChange={(e) => setNominationName(e.target.value)} />
-                <button className="button-create" onClick={handleAddNomination}>Добавить</button>
+                <button className="adminp-button-create" onClick={handleAddNomination}>Добавить</button>
               </div>
               <div>
                 {nominations.map((nomination, index) => (
                   <div key={index}>
-                    <div className="nomination-container">
+                    <div className="adminp-nomination-container">
                       <div>{nomination}</div>
-                      <button className="button-delete" onClick={() => handleDeleteNomination(index)}>Удалить</button>
+                      <button className="adminp-button-delete" onClick={() => handleDeleteNomination(index)}>Удалить</button>
                     </div>
                   </div>
                 ))}
