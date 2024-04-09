83e6cde17516d2765780e70d2c8800aca238913f
Demos101 <amirov_2001@mail.ru>
2024-04-09 10:44:41 +0500

    fix(betabeta)

--- front/site-body/src/request_page/RequestPage.js
+++ front/site-body/src/request_page/RequestPage.js
@@ -4,7 +4,8 @@ import './RequestPage.css';
 const RequestPage = () => {
   return (
     <div>
-      <div className="it-vesna">IT Весна</div>
+      <div className="it-vesna">IT Весна
+      </div>
       <div className="zayavka-container">
         <div className="left-container">
           <ZayavkaLeft />
@@ -19,51 +20,53 @@ const RequestPage = () => {
 };
 
 const ZayavkaLeft = () => {
-    const [fileAttached, setFileAttached] = useState(false);
-    const [fileName, setFileName] = useState('');
-  
-    const handleFileUpload = (event) => {
-      const file = event.target.files[0];
-      if (file) {
-        setFileAttached(true);
-        setFileName(file.name);
-      } else {
-        setFileAttached(false);
-        setFileName('');
-      }
-    };
-  
-    return (
-      <div className="left-content" style={{ height: '100%' }}>
-        <h2> </h2>
-        <div>
-          <input type="text" placeholder="Авторы" />
-        </div>
-        <div>
-          <input type="text" placeholder="Возраст" />
-        </div>
-        <div>
-          <input type="email" placeholder="Почта" />
-        </div>
-        <div>
-          <label htmlFor="nomination">Номинация:</label>
-          <select id="nomination">
-            <option value="nomination1">Номинация 1</option>
-            <option value="nomination2">Номинация 2</option>
-            <option value="nomination3">Номинация 3</option>
-          </select>
-        </div>
-        <div>
-          <input type="text" placeholder="Учреждение" />
-        </div>
-        <div>
-          <label htmlFor="file-upload" style={{ display: fileAttached ? 'none' : 'block' }}>Прикрепить согласие</label>
-          <input type="file" id="file-upload" style={{ display: 'none' }} onChange={handleFileUpload} />
-          {fileAttached && <div>{fileName}</div>}
-        </div>
-      </div>
-    );
+  const [fileAttached, setFileAttached] = useState(false);
+  const [fileName, setFileName] = useState('');
+
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setFileAttached(true);
+      setFileName(file.name);
+    } else {
+      setFileAttached(false);
+      setFileName('');
+    }
   };
+
+  return (
+    <div className="left-content" style={{ height: '100%' }}>
+      <h2></h2>
+      <div>
+        <input type="text" className="input-text" placeholder="Авторы" />
+      </div>
+      <div>
+        <input type="text" className="input-text" placeholder="Возраст" />
+      </div>
+      <div>
+        <input type="email" className="input-email" placeholder="Почта" />
+      </div>
+      <div>
+        <label htmlFor="nomination">Номинация:</label>
+        <select id="nomination">
+          <option value="nomination1">Номинация 1</option>
+          <option value="nomination2">Номинация 2</option>
+          <option value="nomination3">Номинация 3</option>
+        </select>
+      </div>
+      <div>
+        <input type="text" className="input-text" placeholder="Учреждение" />
+      </div>
+      <div>
+        <label htmlFor="file-upload" style={{ display: fileAttached ? 'none' : 'block' }}>
+          Прикрепить согласие
+        </label>
+        <input type="file" id="file-upload" style={{ display: 'none' }} onChange={handleFileUpload} />
+        {fileAttached && <div>{fileName}</div>}
+      </div>
+    </div>
+  );
+};
 
   const ZayavkaRight = () => {
     return (
