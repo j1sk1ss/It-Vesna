ab585f644698d0ef3694419b83717f9cd3785be3
j1sk1ss <fotikolay222@gmail.com>
2024-04-17 15:30:06 +0500

    API finished

--- back/api_service/service.py
+++ back/api_service/service.py
@@ -13,7 +13,9 @@ db_get_posts_by_id, db_pin_post, db_unpin_post
 from data_base.apl_data_base import db_add_application, db_delete_application, db_delete_application_in_approved, \
 db_delete_application_in_archive, db_get_approved, db_get_archives, db_get_application, db_get_applications, \
 db_application2approved, db_application2archive
+from data_base.nom_data_base import db_add_nomination, db_delete_nomination, db_get_nomination, db_get_nominations
 
+from file.file import file_get, file_put
 from mail.mail import ml_send_mail
 from crypto.crypto import crypto_str2hash
 
@@ -50,7 +52,7 @@ def limit_remote_addr():
         return 'ip not allowed'
 
 
-
+#region [Users]
 # =============================================================
 #   Users DB API
 # =============================================================
@@ -121,9 +123,9 @@ def get_user():
         return db_get_user_by_id(data['ID'])
     else:
         return db_get_user_by_mail(data['Mail'])
+#endregion
 
-
-
+#region [Passwords]
 # =============================================================
 #   Passwords DB API
 # =============================================================
@@ -179,9 +181,9 @@ def str2hash():
     }
 
     return ret_data
+#endregion
 
-
-
+#region [Nominations]
 # =============================================================
 #   Notifications DB API
 # =============================================================
@@ -207,9 +209,9 @@ def add_notificated():
 @app.route('/api/notificated/<int:user_id>', methods=['DELETE'])
 def delete_notificated(user_id):
     return db_delete_notify(user_id)
+#endregion
 
-
-
+#region [Moders]
 # =============================================================
 #   Moderators DB API
 # =============================================================
@@ -273,9 +275,9 @@ def delete_moderator(user_id):
 def send_mail():
     data = request.json
     return ml_send_mail(data['destination'], data['header'], data['text'], data['type'])
+#endregion
 
-
-
+#region [Posts]
 # =============================================================
 #   Posts API
 # =============================================================
@@ -294,6 +296,7 @@ def create_post():
     data = request.json
     return db_add_post(data['author'], data['path'], data['category'])
 
+
 # ============================
 # Delete post
 # DELETE  http://it-vesna-api-service-1:27001/api/posts/<int:post_id>
@@ -301,6 +304,7 @@ def create_post():
 @app.route('/api/posts/<int:post_id>', methods=['DELETE'])
 def delete_post(post_id):
     return db_delete_post(post_id)
+
 
 # ============================
 # Get all posts
@@ -317,6 +321,7 @@ def delete_post(post_id):
 def get_all_posts():
     return db_get_posts()
 
+
 # ============================
 # Get posts by categy
 # GET http://it-vesna-api-service-1:27001/api/posts/<string:category>
@@ -331,6 +336,7 @@ def get_all_posts():
 @app.route('/api/posts/<string:category>', methods=['GET'])
 def get_posts_by_category(category):
     return db_get_posts_by_category(category)
+
 
 # ============================
 # Get post by post id
@@ -347,75 +353,235 @@ def get_posts_by_category(category):
 def get_posts_by_id(id):
     return db_get_posts_by_id(id)
 
+
 # ============================
 # Pin post
-# POST http://it-vesna-api-service-1:27001/posts/pinned/<int:post_id>
+# POST http://it-vesna-api-service-1:27001/api/posts/pinned/<int:post_id>
 # RETURN: "success" / "not found"
 @app.route('/api/posts/pin/<int:id>', methods=['POST'])
 def pin_post(id):
     return db_pin_post(id)
 
+
 # ============================
 # Unpin post
-# POST http://it-vesna-api-service-1:27001/posts/unpinned/<int:post_id>
+# POST http://it-vesna-api-service-1:27001/api/posts/unpinned/<int:post_id>
 # RETURN: "success" / "not found"
 @app.route('/api/posts/pin/<int:id>', methods=['DELETE'])
 def unpin_post(id):
     return db_unpin_post(id)
+#endregion
 
-
-
+#region [Applications]
 # =============================================================
 #   Applications API
 # =============================================================
 
+# ============================
+# Create application 
+# POST http://it-vesna-api-service-1:27001/api/applications
+# JSON request: {
+#     "user_id": "user_id",
+#     "name": "name",
+#     "path": "path"
+# }
+# RETURN: "success"
+@app.route('/api/applications', methods=['POST'])
 def create_application():
     data = request.json
     return db_add_application(data['user_id'], data['name'], data['path'])
 
+
+# ============================
+# Delete application 
+# POST http://it-vesna-api-service-1:27001/api/applications/<int:user_id>
+# RETURN: "success" / "not found"
+@app.route('/api/applications/<int:user_id>', methods=['DELETE'])
 def delete_application(user_id):
     return db_delete_application(user_id)
 
+
+# ============================
+# This function return all applications from users from db
+# GET http://it-vesna-api-service-1:27001/api/applications
+# No JSON request required for GET application
+# RETURN: applications
+# [{
+#   'UID': 'User_UID',
+#   'Path': 'Path_to_request_on_local'
+# }, ... ]
+@app.route('/api/applications', methods=['GET'])
 def get_applications():
     return db_get_applications()
 
+
+# ============================
+# Get application by user_id
+# GET http://it-vesna-api-service-1:27001/api/applications/user/<int:user_id>
+# RETURN: "success"
+@app.route('/api/applications/user/<int:user_id>', methods=['GET'])
 def get_application_by_id(user_id):
     return db_get_application(user_id, -1)
 
+
+# ============================
+# Get application by user_id
+# GET http://it-vesna-api-service-1:27001/api/applications/<int:application_id>
+# RETURN: "success"
+@app.route('/api/applications/<int:application_id>', methods=['GET'])
 def get_application_by_apl_id(application_id):
     return db_get_application(-1, application_id)
 
+
+# ============================
+# Add application to archive
+# POST http://it-vesna-api-service-1:27001/api/applications/archive/<int:application_id>
+# JSON application: {
+#     "ID": "application id"
+# }
+# RETURN: "success"
+@app.route('/api/applications/archive/<int:application_id>', methods=['POST'])
 def application2archive(application_id):
     return db_application2archive(application_id)
 
+
+# ============================
+# Delete application from archive
+# DELETE http://it-vesna-api-service-1:27001/api/applications/archive/<int:application_id>
+# RETURN: "success"
+@app.route('/api/applications/archive/<int:application_id>', methods=['DELETE'])
 def application_from_archive(application_id):
     return db_delete_application_in_archive(application_id)
 
+
+# ============================
+# This function return all archive from users from db
+# GET http://it-vesna-api-service-1:27001/api/applications/archive
+# No JSON request required for GET application
+# RETURN: application
+# [{
+#   'UID': 'applicationid',
+# }, ... ]
+@app.route('/api/applications/archive', methods=['GET'])
 def get_archive():
     return db_get_archives()
 
+
+# ============================
+# Add application to approved
+# POST http://it-vesna-api-service-1:27001/api/applications/approve/<int:application_id>
+# JSON application: {
+#     "ID": "application id"
+# }
+# RETURN: "success"
+@app.route('/api/applications/approve/<int:application_id>', methods=['POST'])
 def application2approved(application_id):
     return db_application2approved(application_id)
 
-def application_from_archive(application_id):
+
+# ============================
+# Delete application from approve
+# DELETE http://it-vesna-api-service-1:27001/api/applications/approve/<int:application_id>
+# RETURN: "success"
+@app.route('/api/applications/approve/<int:application_id>', methods=['DELETE'])
+def application_from_approved(application_id):
     return db_delete_application_in_approved(application_id)
 
+
+# ============================
+# This function return all approve from users from db
+# GET http://it-vesna-api-service-1:27001/api/applications/approve
+# No JSON request required for GET application
+# RETURN: application
+# [{
+#   'UID': 'applicationid',
+# }, ... ]
+@app.route('/api/applications/approved', methods=['GET'])
 def get_approved():
     return db_get_approved()
+#endregion
 
-
+#region [Nominations]
 # =============================================================
 #   Nominations API
 # =============================================================
-# TODO: Complete
+
+# ============================
+# Add nomination
+# POST http://it-vesna-api-service-1:27001/api/nominations
+# JSON request: {
+#     "Name": "NominationName"
+# }
+@app.route('/api/nominations', methods=['POST'])
+def add_nomination():
+    data = request.json
+    return db_add_nomination(data['name'])
 
 
+# ============================
+# Delete nomination
+# DELETE http://it-vesna-api-service-1:27001/api/nominations/<int:nomination_id>
+# No JSON request required for DELETE request
+@app.route('/api/nominations/<int:nomination_id>', methods=['DELETE'])
+def delete_nomination(nomination_id):
+    return db_delete_nomination(nomination_id)
 
+
+# ============================
+# This function return all nominations from db
+# GET http://it-vesna-api-service-1:27001/api/nominations
+# No JSON request required for GET request
+# RETURN: nom
+# [{
+#   'UID': 'nomination_id',
+#   'Path': 'nomination_name'
+# }, ... ]
+@app.route('/api/nominations', methods=['GET'])
+def get_nominations():
+    return db_get_nominations()
+
+
+# ============================
+# Get nomination by id
+# GET http://it-vesna-api-service-1:27001/api/nominations/<int:user_id>
+# RETURN: "success"
+@app.route('/api/nominations/<int:nomination_id>', methods=['GET'])
+def get_nomination(nomination_id):
+    return db_get_nomination(nomination_id)
+#endregion
+
+#region [Files]
 # =============================================================
 #   Files API
 # =============================================================
-# TODO: Complete
 
+# ==================
+# Save transfered file
+# POST http://it-vesna-api-service-1:27001/api/file
+# JSON request: {
+#     "path": "path"
+# }
+# FILE ATTACHMENT REQUIRED
+# RETURN: "no file" / file path
+@app.route('/api/file', methods=['POST'])
+def put_file():
+    data = request.json
+    file = request.files['file']
+    return file_put(file, data['path'])
+
+
+# ==================
+# Load file from server
+# GET http://it-vesna-api-service-1:27001/api/file
+# JSON request: {
+#     "path": "file_path"
+# }
+# RETURN: file
+@app.route('/api/file', methods=['GET'])
+def get_file():
+    data = request.json
+    return file_get(data['path'])
+#endregion
 
 
 # =============================================================
