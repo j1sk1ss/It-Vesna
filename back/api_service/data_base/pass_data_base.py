fa5b8a82601f2cb551a8397564c64ebbab58d708
j1sk1ss <fotikolay222@gmail.com>
2024-04-28 13:27:47 +0500

    pass fix

--- back/api_service/data_base/pass_data_base.py
+++ back/api_service/data_base/pass_data_base.py
@@ -13,4 +13,4 @@ def db_change_password(pass_hash, pass_salt, user_id):
     return requests.put(f'{DB_SERVER}/passwords/{user_id}', json=send_data, headers={'Content-Type': 'application/json'}).text
 
 def db_get_password(user_id):
-    return requests.get(f'{DB_SERVER}/passwords/{user_id}').text
+    return requests.get(f'{DB_SERVER}/passwords/{user_id}')
