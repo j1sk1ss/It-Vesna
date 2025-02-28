3c7dcb99497efae4e4dd8e9117cde3be3146926c
nikolaj <fotikolay222@gmail.com>
2025-02-28 12:02:51 +0500

    project refactor

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-from flask import Flask, render_template
 import os
+from flask import Flask, render_template, abort
 
-app = Flask(__name__, template_folder="front/new_struct/site_body/src")
 
-@app.route('/')
-def serve_index():
-    return render_template('App.html')
+def _find_templates() -> dict:
+    routes: dict = {}
+    for root, _, files in os.walk(app.template_folder):
+        for file in files:
+            if file.endswith(".html"):
+                rel_path = os.path.relpath(os.path.join(root, file), app.template_folder)
+                route = "/" + rel_path.replace("\\", "/").replace(".html", "")
+                routes[route] = rel_path.replace("\\", "/")
+    return routes
+
+
+app: Flask = Flask(__name__, template_folder="templates", static_folder="static")
+TEMPLATES = _find_templates()
+
+
+@app.route('/<path:subpath>')
+def dynamic_route(subpath: str):
+    template_path = TEMPLATES.get("/" + subpath)
+    if template_path:
+        return render_template(template_path)
+    
+    abort(404)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+    
--- back/psql/posts_service/init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Post {
-    UID SERIAL PRIMARY KEY,
-    Author_UID INT,
-    PostPath VARCHAR(255) NOT NULL,
-    Pinned INT,
-    Category VARCHAR(255) NOT NULL,
-};
